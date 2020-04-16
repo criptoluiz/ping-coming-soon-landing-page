1e12e5f8defa3a05df01208e010ee79d637af9d1
luigi <luiz.pb@cryptolab.net>
2020-04-16 17:44:12 +0000

    js

---
+++ script.js
@@ -0,0 +1,22 @@
+const label = document.getElementById("label");
+const email = document.getElementById("email");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const emailValue = email.value;
+
+  if (!isValid(emailValue)) {
+    label.classList.remove("form_error")
+    label.classList.add("form-error");
+    
+  } else {
+    label.classList.remove("form-error");
+    label.classList.add("form_error")
+  }
+});
+
+function isValid(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
